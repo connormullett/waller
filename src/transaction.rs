2f08ca2b7c4332a3ea28f1f3826af39880bdf6cc
connor mullett <connormullett@gmail.com>
2022-05-28 23:48:05 -0400

    basic implementations of RawTransaction

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,21 +1,84 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+#[derive(Debug, Clone)]
+pub enum TransactionVersion {
+    One,
+}
+
 /// a transaction in Raw format
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct RawTransaction {
     /// transaction version number. currently either 1 or 2
-    version: i32,
-    /// the number of inputs in this transaction
-    tx_in_count: usize,
+    version: TransactionVersion,
     /// the inputs to the transaction
     tx_in: Vec<TransactionInput>,
-    /// number of outputs in this transaction
-    tx_out_count: usize,
     /// vector of transaction outputs
     tx_out: Vec<TransactionOutput>,
     /// a timestamp or block number
-    lock_time: u32,
+    lock_time: u128,
 }
 
-#[derive(Debug)]
+impl RawTransaction {
+    pub fn new(
+        inputs: Vec<TransactionInput>,
+        outputs: Vec<TransactionOutput>,
+        lock_time: Option<u128>,
+    ) -> Self {
+        match lock_time {
+            Some(time) => Self {
+                version: TransactionVersion::One,
+                tx_in: inputs,
+                tx_out: outputs,
+                lock_time: time,
+            },
+            None => {
+                let start = SystemTime::now();
+                Self {
+                    version: TransactionVersion::One,
+                    tx_in: inputs,
+                    tx_out: outputs,
+                    lock_time: start
+                        .duration_since(UNIX_EPOCH)
+                        .expect("time went backwards")
+                        .as_millis(),
+                }
+            }
+        }
+    }
+
+    pub fn tx_id(&self) -> String {
+        // hash all tx data with sha256 twice
+        todo!()
+    }
+
+    pub fn version(&self) -> TransactionVersion {
+        self.version.clone()
+    }
+
+    pub fn inputs(&self) -> Vec<TransactionInput> {
+        self.tx_in.clone()
+    }
+
+    pub fn outputs(&self) -> Vec<TransactionOutput> {
+        self.tx_out.clone()
+    }
+
+    /// The amount of inputs in the transaction
+    pub fn tx_in_count(&self) -> usize {
+        self.tx_in.len()
+    }
+
+    /// The amount of outputs in the transaction
+    pub fn tx_out_count(&self) -> usize {
+        self.tx_out.len()
+    }
+
+    pub fn lock_time(&self) -> u128 {
+        self.lock_time
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct TransactionInput {
     /// previous output being spent
     previous_output: OutPoint,
@@ -32,7 +95,7 @@ pub struct TransactionInput {
 /// a tx can have multiple outputs so the Outpoint
 /// includes a txid and an output index to refer
 /// to a specific output
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct OutPoint {
     /// the TXID of the tx holding the output to spend
     hash: String,
@@ -45,7 +108,7 @@ pub struct OutPoint {
 /// each output spends a certain number of sats
 /// placing them under control of anyone who can
 /// satisfy the provided pubkey script
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct TransactionOutput {
     /// number of satoshis to spend
     value: i64,
